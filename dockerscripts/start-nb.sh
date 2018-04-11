85cb131784430a7becef90fe8d10cd0e4fa5cba8
Kat Petre <kapetre@gmail.com>
2018-04-11 04:37:03 -0700

    captain obvious

--- dockerscripts/start-nb.sh
+++ dockerscripts/start-nb.sh
@@ -2,7 +2,7 @@
 
 jupyter notebook \
 --NotebookApp.ip=0.0.0.0 \
---NotebookApp.token='ethdemos' \
+--NotebookApp.token='thereisnospoon' \
 --NotebookApp.iopub_msg_rate_limit=10000 \
 --NotebookApp.port=8895 \
 --FileManagerMixin.use_atomic_writing=False
